a486bce6b240d2a32965712991303ab84a1bf4e5
family93 <601610870@qq.com>
2018-08-12 16:43:06 +0800

    Add springboot-mybatis project

---
+++ springboot-mybatis/src/main/java/com/makesailing/neo/repository/UserMapper.java
@@ -0,0 +1,24 @@
+package com.makesailing.neo.repository;
+
+import com.makesailing.neo.domain.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * #
+ *
+ * @Author: jamie.li
+ * @Date: Created in  2018/8/12 16:22
+ */
+@Mapper
+public interface UserMapper {
+
+  @Select("select * from user where name = #{name}")
+  User getUserByName(@Param("name") String name);
+
+  @Insert("insert into user (name ,age) values ( #{name},#{age})")
+  int insertUser(@Param("name") String name, @Param("age") Integer age);
+
+}
