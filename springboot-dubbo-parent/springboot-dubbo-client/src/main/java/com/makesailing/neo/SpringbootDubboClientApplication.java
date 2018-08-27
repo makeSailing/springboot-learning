0ab9f7dfe81f2cde0d7877d1115c7ff05146c8e7
jamie.li <jamie.li@wolaidai.com>
2018-08-27 18:31:51 +0800

    Add dubbo-consumer.xml

--- springboot-dubbo-parent/springboot-dubbo-client/src/main/java/com/makesailing/neo/SpringbootDubboClientApplication.java
+++ springboot-dubbo-parent/springboot-dubbo-client/src/main/java/com/makesailing/neo/SpringbootDubboClientApplication.java
@@ -2,8 +2,10 @@ package com.makesailing.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:dubbo-consumer.xml")
 public class SpringbootDubboClientApplication {
 
 	public static void main(String[] args) {
---
+++ springboot-dubbo-parent/springboot-dubbo-client/src/main/java/com/makesailing/neo/controller/ItemController.java
@@ -0,0 +1,27 @@
+package com.makesailing.neo.controller;
+
+import com.makesailing.neo.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * #
+ *
+ * @author <a href="mailto:jamie.li@wolaidai.com">jamie.li</a>
+ * @date 2018/8/27 18:11
+ */
+@RestController
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("/canbuy")
+	public String canBuy(@RequestParam("id") String id){
+		return itemService.checkItemStatus(id) ? "can buy!" : "can not buy!";
+	}
+}
+
+
